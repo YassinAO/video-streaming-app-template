7f24d793857e5f644cffd4aba5362550ccb3a483
Y4SSIN <yassin_developer@outlook.com>
2019-12-25 17:24:52 +0100

    Display user channel

---
+++ resources/views/channels/show.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    {{ $channel->name }}
+                </div>
+
+                <div class="card-body">
+                    <form id="update-channel-form" action="{{ route('channels.update', $channel->id) }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PATCH')
+
+                    <div class="form-group row justify-content-center">
+                        <div class="channel-avatar">
+                            <div onclick="document.querySelector('#image').click()" class="channel-avatar-overlay">
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <input onchange="document.querySelector('#update-channel-form').submit()" style="display: none;" id="image" type="file" name="image">
+
+                    <div class="form-group">
+                        <label for="name" class="form-control-label">
+                            Name
+                        </label>
+                        <input id="name" name="name" value="{{$channel->name}}"type="text" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="description" class="form-control-label">
+                            Description
+                        </label>
+                        <textarea name="description" id="description" cols="3" rows="3" class="form-control">
+                            {{ $channel->description }}
+                        </textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-info">
+                        Update Channel
+                    </button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('channels', 'ChannelController');
