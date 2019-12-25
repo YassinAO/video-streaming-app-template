f32eb557eebcc04fb3b3d361b9809e51ced81302
Y4SSIN <yassin_developer@outlook.com>
2019-12-25 15:50:20 +0100

    ID to UUID

---
+++ app/Model.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model as BaseModel;
+
+class Model extends BaseModel
+{
+    public $incrementing = false;
+
+    protected static function boot(){
+        parent::boot();
+
+        static::creating(function($model){
+            $model->{$model->getKeyName()} = Str::uuid();
+        });
+    }
+}
--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Str;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -9,6 +10,16 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
+
+    public $incrementing = false;
+
+    protected static function boot(){
+        parent::boot();
+
+        static::creating(function($model){
+            $model->{$model->getKeyName()} = (string)  Str::uuid();
+        });
+    }
 
     /**
      * The attributes that are mass assignable.
