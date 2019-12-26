7025f7cfcb4ad783fdaee3762797bbc674b421dc
Y4SSIN <yassin_developer@outlook.com>
2019-12-26 11:43:22 +0100

    Formatting the subscriber count

--- resources/js/components/subscribe-button.js
+++ resources/js/components/subscribe-button.js
@@ -1,3 +1,5 @@
+import numeral from 'numeral'
+
 Vue.component('subscribe-button', {
 
     props: {
@@ -25,6 +27,10 @@ Vue.component('subscribe-button', {
             if (__auth() && this.channel.user_id === __auth().id) return true
 
             return false
+        },
+
+        count() {
+            return numeral(this.subscriptions.length).format('0a')
         }
     },
 
